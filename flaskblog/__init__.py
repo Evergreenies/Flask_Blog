6b3d584b939666933576c03b68f253f68b60ea0c
Suyog Shimpi <evergreenlief@gmail.com>
2019-05-02 17:16:00 -0400

    Update run.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -17,12 +17,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-# app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-
-# Just demo
-app.config['MAIL_USERNAME'] = 'evergreenlief@gmail.com'
-app.config['MAIL_PASSWORD'] = '@Montya28.'
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 
 mail = Mail(app)
 
